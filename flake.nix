bc645e76a9cc9d5a28dce941644e6b11398b1cc3
georgeee <george.agapov@gmail.com>
2024-05-01 19:28:17 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -1,12 +1,16 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, flake-utils }: {
     overlay = final: prev: {
       describe-dune = final.ocamlPackages.callPackage ./describe-dune.nix { };
     };
-    packages.x86_64-linux.describe-dune =
-      (nixpkgs.legacyPackages.x86_64-linux.extend self.overlay).describe-dune;
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.describe-dune;
-  };
+  } // flake-utils.lib.eachDefaultSystem (system:
+    {
+      packages."${system}".describe-dune =
+        (nixpkgs.legacyPackages."${system}".extend self.overlay).describe-dune;
+      defaultPackage."${system}" = self.packages."${system}".describe-dune;
+    }
+  );
 }
